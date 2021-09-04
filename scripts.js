baae350e3a01eae12750a5503e28749b5acc3e7a
Emerson916 <emersons.a296@gmail.com>
2021-09-03 23:37:19 -0300

    Finalização do projeto

--- scripts.js
+++ scripts.js
@@ -1,35 +1,52 @@
 //Pegamos os elementos html e checkbox
 const html = document.querySelector("html");
-const checkbox = document.querySelector("input [name=theme]");
+const checkbox = document.querySelector("input[name=theme]");
 
 const getStyle = (element, style) =>
-    window.getComputedStyle(element).getPropertyValue(style)
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
     
 const initialColors = {
     //Pegamos as cores do CSS direto pelo HTML
-    background: getStyle(html,"--bg"),
-    backgroundPanel:getStyle(html, "--bg-panel"),
+    bg: getStyle(html,"--bg"),
+    bgPanel:getStyle(html, "--bg-panel"),
     colorHeadings: getStyle(html, "--color-headings"),
     colorText: getStyle(html, "--color-text")
 }
 
-const dackColors = {
+const darkColors = {
     //Colocamos as cores do darkMode
-    background: "#333333",
-    backgroundPanel:"#434343",
+    bg: "#333333",
+    bgPanel:"#434343",
     colorHeadings: "#3664FF",
     colorText: "#B5B5B5"
 }
 
+/* Fomatando as chaves para alterar a cor no HTML/CSS */
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
 const changeColors = (colors) => {
-    console.log(colors)
+   Object.keys(colors).map(key =>
+        html.style.setProperty(transformKey(key), colors[key])
+    )
 }
 
 //Adicionamos ações quando ouver "mudanças - change" no target
+
 checkbox.addEventListener("change", ({target}) => {
-    if(target === checked){
-        changeColors();
+    target.checked ? changeColors(darkColors) : changeColors(initialColors)
+})
+
+
+checkbox.addEventListener("change", ({target}) => {
+/* target.checked ? changeColors(darkColors) : changeColors(initialColors);*/
+    if(checkbox.checked){
+        changeColors(darkColors);
     }else{
-        changeColors();
+        changeColors(initialColors);
     }
+    
 })
